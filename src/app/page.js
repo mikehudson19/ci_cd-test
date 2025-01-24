c1e0c8adcebf1ca87ee05548ff4b69f9fe2f5925
mikeh <mikehudson19@live.co.uk>
2025-01-24 16:24:47 +0200

    Change to deploy pipeline

--- src/app/page.js
+++ src/app/page.js
@@ -23,6 +23,7 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
           <li>Change from test branch</li>
           <li>Small change</li>
+          <li>Another Small change</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
